f7f4e1603f137f379d62609a06f3877d0103d0ea
longlin20 <48489753+longlin20@users.noreply.github.com>
2019-10-04 09:16:47 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+  int i = 0;
+    cout << "Hello Wordl!! Is my firt repository in GitHub."
+  return 0;
+}
