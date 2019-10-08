b106a1a4314793f4c14f641cbaea8ffd30a95879
dariaok <56162704+dariaok@users.noreply.github.com>
2019-10-08 17:36:43 +0200

    small change in the level.java

--- src/Level.java
+++ src/Level.java
@@ -1,16 +1,28 @@
 // Es un clase enumerada con la que se encapsulan los tres niveles de juego.
 
 public class Level{
+	difficulty diff;
 	
 	private enum difficulty{
 		easy, normal, hard
 		}
 	
+	public enum setDifficulty(int dif) {
+		if(dif == 1){
+			diff = easy;	
+		}
+		else if(dif == 2){
+			diff = normal;
+		}
+		else if(dif == 3){
+			diff = hard;
+		}
+		
+		//or in that case when the argument is ENUM
+		// diff = diff
+	}
+	
 	public enum getDifficulty() {
 		return difficult;
 	}
-		
-	//}
-	
-	
 }
